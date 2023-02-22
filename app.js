6ba0358c913466892ff8a670104444cf58965ba3
DarcianD <darciandinh@gmail.com>
2023-02-21 18:24:50 -0600

    added slider, change size func,and black colormouse over

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-let container = document.querySelector(".container");
-container.style.gridTemplateColumns = "repeat(16,1fr)";
-container.style.gridTemplateRows= "repeat(16,1fr)";
+function populateContainer(num){
+    let container = document.querySelector(".container");
+    let sizes = container.querySelectorAll("div");
+    sizes.forEach((div) => div.remove()) ;
+    container.style.gridTemplateColumns = `repeat(${num},1fr)`;
+    container.style.gridTemplateRows= `repeat(${num},1fr)`;
 
-for (let i = 0; i < 256; i++){
-    let grid = document.createElement("div")
-    grid.style.backgroundColor = "Gray";
-    container.insertAdjacentElement("beforeend",grid);
+    let sum = num * num;
+    for (let i = 0; i < sum; i++){  
+        let grid = document.createElement("div")
+        grid.addEventListener("mouseover", () => { 
+            grid.style.backgroundColor = "black";
+        })
+        grid.style.backgroundColor = "white"; 
+        grid.style.border = "1px solid gray"
+        container.insertAdjacentElement("beforeend",grid);
+}};
+
+function changesSize(input){
+    if (input >=10 || input <=100){
+        populateContainer(input);
+    } else{
+        
+    }
 }
+
+populateContainer(10);
